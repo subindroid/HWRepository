1a6f3fe22261a38bb21a818bbfd0c7e22a8ee920
김수빈 <@Sk>
2025-12-14 21:13:21 +0900

    First commit in alice branch-modify A.java and C.java

--- GitHW/src/mygit/A.java
+++ GitHW/src/mygit/A.java
@@ -2,6 +2,7 @@ package mygit;
 
 public class A {
 	public void function_A(String param_A) {
-		System.out.println("This is function_A: " + param_A);
+		System.out.println("This is function_A");
+		System.out.println(param_A);
 	}
 }
--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -3,6 +3,8 @@ package mygit;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("Main");
+		A a = new A();
+		a.function_A("cat");
 	}
 
 }
