a003621d463b1334eb9dbfab16f89f1fdf00d775
김수빈 <@Sk>
2025-12-14 21:16:30 +0900

    Modify C.java

--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -3,7 +3,7 @@ package mygit;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("Main");
-		System.out.println("C.java change in master branch")
+		System.out.println("C.java change in master branch");
 	}
 
 }
