72e64563f11ebd8ccb258194665664655d1251da
김수빈 <@Sk>
2025-12-14 21:18:47 +0900

    First commit in Bob brench

--- GitHW/src/mygit/B.java
+++ GitHW/src/mygit/B.java
@@ -2,6 +2,7 @@ package mygit;
 
 public class B {
 	public void function_B(String param_B) {
-		System.out.println("This is function_B: " + param_B);
+		System.out.println("This is function_B");
+		System.out.println(param_B);
 	}
 }
